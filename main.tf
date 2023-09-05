4b53b834271cc06551889990875db8751409bc6a
piyush168713 <20BCS9107@cuchd.in>
2023-09-05 11:21:01 +0530

    tf

--- main.tf
+++ main.tf
@@ -1,20 +1,24 @@
-provider "aws" {}
+provider "aws" {
+    region = "ap-south-1"
+}
 
-variable "cidr_blocks" {}
-variable "avail_zone" {}
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
 
 resource "aws_vpc" "myapp-vpc" {
-    cidr_block = var.cidr_blocks[0].cidr_block
+    cidr_block = var.vpc_cidr_block
     tags = {
-        Name: var.cidr_blocks[0].name
+        Name: "${var.env_prefix}-vpc"
     }
 }
 
 resource "aws_subnet" "myapp-subnet-1" {
-    vpc_id = aws_vpc.dev-vpc.id
-    cidr_block = var.cidr_blocks[1].cidr_block
-    availability_zone = "ap-south-1a"
+    vpc_id = aws_vpc.myapp-vpc.id
+    cidr_block = var.subnet_cidr_block
+    availability_zone = var.avail_zone
     tags = {
-        Name: var.cidr_blocks[1].name
+        Name: "${var.env_prefix}-subnet-1"
     }
 }
